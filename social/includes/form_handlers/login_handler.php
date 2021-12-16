52600f25b5463b335affe9eb50d8b945fc0dce8a
Jun <42493669+JunJosh@users.noreply.github.com>
2021-12-16 09:13:42 +0700

    Update login_handler.php

--- social/includes/form_handlers/login_handler.php
+++ social/includes/form_handlers/login_handler.php
@@ -3,8 +3,8 @@
 if(isset($_POST['login_button'])) {
 	$email = filter_var($_POST['log_email'], FILTER_SANITIZE_EMAIL);
 
-	$_SESSION['log_email'] = $email;
-	$password = md5($_POST['log_password']);
+	$_SESSION['log_email'] = $email; //Store email into session variable
+	$password = md5($_POST['log_password']); //Get password
 
 	$check_database_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND password='$password'");
 	$check_login_query = mysqli_num_rows($check_database_query);
@@ -12,6 +12,11 @@ if(isset($_POST['login_button'])) {
 	if($check_login_query == 1) {
 		$row = mysqli_fetch_array($check_database_query);
 		$username = $row['username'];
+
+		$user_closed_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND user_closed='yes'");
+		if(mysqli_num_rows($user_closed_query) == 1 ){
+			$reopen_account = mysqli_query($con, "UPDATE users SET user_closed='no' WHERE email='$email'");
+		}
 
 		$_SESSION['username'] = $username;
 		header("Location: index.php");
